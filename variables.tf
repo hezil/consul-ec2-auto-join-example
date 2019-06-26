fc53041dcd1e22e4c9e8daa262ba6b890e5fa133
hezil <31658384+hezil@users.noreply.github.com>
2019-06-26 22:48:05 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -40,12 +40,12 @@ variable "consul_version" {
 
 variable "vpc_cidr_block" {
   description = "The top-level CIDR block for the VPC."
-  default     = "10.1.0.0/16"
+  default     = "10.88.0.0/16"
 }
 
 variable "cidr_blocks" {
   description = "The CIDR blocks to create the workstations in."
-  default     = ["10.1.1.0/24", "10.1.2.0/24"]
+  default     = ["10.88.1.0/24", "10.88.2.0/24"]
 }
 
 variable "consul_join_tag_key" {
