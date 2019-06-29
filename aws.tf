5f9db67c69c6011719d8714524a5c7648a6af556
hezil <31658384+hezil@users.noreply.github.com>
2019-06-29 22:26:06 +0300

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -66,8 +66,8 @@ resource "aws_subnet" "consul" {
   }
 }
 
-resource "aws_security_group" "opsschool_consul" {
-  name        = "opsschool-consul"
+resource "aws_security_group" "final_project" {
+  name        = "final_project"
   description = "Allow ssh & consul inbound traffic"
 
   ingress {
@@ -124,27 +124,27 @@ resource "aws_security_group" "opsschool_consul" {
 
 # Create an IAM role for the auto-join
 resource "aws_iam_role" "consul-join" {
-  name               = "opsschool-consul-join"
+  name               = "final_project-consul-join"
   assume_role_policy = "${file("${path.module}/templates/policies/assume-role.json")}"
 }
 
 # Create the policy
 resource "aws_iam_policy" "consul-join" {
-  name        = "opsschool-consul-join"
+  name        = "final_project-consul-join"
   description = "Allows Consul nodes to describe instances for joining."
   policy      = "${file("${path.module}/templates/policies/describe-instances.json")}"
 }
 
 # Attach the policy
 resource "aws_iam_policy_attachment" "consul-join" {
-  name       = "opsschool-consul-join"
+  name       = "final_project-consul-join"
   roles      = ["${aws_iam_role.consul-join.name}"]
   policy_arn = "${aws_iam_policy.consul-join.arn}"
 }
 
 # Create the instance profile
 resource "aws_iam_instance_profile" "consul-join" {
-  name  = "opsschool-consul-join"
+  name  = "final_project-consul-join"
   role = "${aws_iam_role.consul-join.name}"
 }
 
