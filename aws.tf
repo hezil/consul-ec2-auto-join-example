d73da35d24908c7c011faf9f5be1f94e4ad54b8b
hezil <31658384+hezil@users.noreply.github.com>
2019-06-30 02:13:14 +0300

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -62,13 +62,13 @@ resource "aws_security_group" "consul" {
   name_prefix = "${var.namespace}"
   vpc_id      = "${aws_vpc.consul.id}"
   
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    self        = true
-    description = "Allow all inside security group"
-  }
+#   ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     self        = true
+#     description = "Allow all inside security group"
+#   }
 
   ingress {
     from_port   = 22
@@ -122,7 +122,7 @@ resource "aws_security_group" "consul" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/8"]
+    cidr_blocks = ["10.1.0.0/16"]
   }
 
   egress {
