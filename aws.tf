2db6a935bbbae8be08708aa78b6eacdfda9a7aa0
hezil <31658384+hezil@users.noreply.github.com>
2019-06-29 22:33:50 +0300

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -92,13 +92,14 @@ resource "aws_security_group" "final_project" {
     to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
-    ingress {
+  
+  ingress {
     from_port   = 30036
     protocol    = "TCP"
     to_port     = 30036
     cidr_blocks = ["0.0.0.0/0"]
       
-    ingress {
+  ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
