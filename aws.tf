e2cd37c54ac75d05f8b4e943074c4481b60815b0
hezil <31658384+hezil@users.noreply.github.com>
2019-06-29 18:46:05 +0300

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -75,7 +75,7 @@ resource "aws_security_group" "consul" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8"]
   }
   
   ingress {
