0a0a5c6e7f514fe55f6374a0ddda01061988b435
hezil <31658384+hezil@users.noreply.github.com>
2019-06-29 22:22:55 +0300

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -66,24 +66,26 @@ resource "aws_subnet" "consul" {
   }
 }
 
-# A security group that makes the instances accessible
-resource "aws_security_group" "consul" {
-  name_prefix = "${var.namespace}"
-  vpc_id      = "${aws_vpc.consul.id}"
+resource "aws_security_group" "opsschool_consul" {
+  name        = "opsschool-consul"
+  description = "Allow ssh & consul inbound traffic"
 
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/8"]
+    self        = true
+    description = "Allow all inside security group"
   }
-  
+
   ingress {
     from_port   = 22
-    protocol    = "TCP"
     to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow ssh from the world"
   }
+
   ingress {
     from_port   = 80
     protocol    = "TCP"
@@ -95,13 +97,55 @@ resource "aws_security_group" "consul" {
     protocol    = "TCP"
     to_port     = 30036
     cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
+      
+    ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8"]
   }
+  
+  ingress {
+    from_port   = 8500
+    to_port     = 8500
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow consul UI access from the world"
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    description     = "Allow all outside security group"
+  }
+}
+
+# Create an IAM role for the auto-join
+resource "aws_iam_role" "consul-join" {
+  name               = "opsschool-consul-join"
+  assume_role_policy = "${file("${path.module}/templates/policies/assume-role.json")}"
+}
+
+# Create the policy
+resource "aws_iam_policy" "consul-join" {
+  name        = "opsschool-consul-join"
+  description = "Allows Consul nodes to describe instances for joining."
+  policy      = "${file("${path.module}/templates/policies/describe-instances.json")}"
+}
+
+# Attach the policy
+resource "aws_iam_policy_attachment" "consul-join" {
+  name       = "opsschool-consul-join"
+  roles      = ["${aws_iam_role.consul-join.name}"]
+  policy_arn = "${aws_iam_policy.consul-join.arn}"
+}
+
+# Create the instance profile
+resource "aws_iam_instance_profile" "consul-join" {
+  name  = "opsschool-consul-join"
+  role = "${aws_iam_role.consul-join.name}"
 }
 
 resource "aws_security_group" "elb_security_group" {
@@ -125,33 +169,3 @@ resource "aws_security_group" "elb_security_group" {
   }
 }
 
-resource "aws_key_pair" "consul" {
-  key_name   = "${var.namespace}"
-  public_key = "${file("${var.public_key_path}")}"
-}
-
-# Create an IAM role for the auto-join
-resource "aws_iam_role" "consul-join" {
-  name               = "${var.namespace}-consul-join"
-  assume_role_policy = "${file("${path.module}/templates/policies/assume-role.json")}"
-}
-
-# Create the policy
-resource "aws_iam_policy" "consul-join" {
-  name        = "${var.namespace}-consul-join"
-  description = "Allows Consul nodes to describe instances for joining."
-  policy      = "${file("${path.module}/templates/policies/describe-instances.json")}"
-}
-
-# Attach the policy
-resource "aws_iam_policy_attachment" "consul-join" {
-  name       = "${var.namespace}-consul-join"
-  roles      = ["${aws_iam_role.consul-join.name}"]
-  policy_arn = "${aws_iam_policy.consul-join.arn}"
-}
-
-# Create the instance profile
-resource "aws_iam_instance_profile" "consul-join" {
-  name  = "${var.namespace}-consul-join"
-  roles = ["${aws_iam_role.consul-join.name}"]
-}
